d7e7bb1b737b91b65ccf50ff6dd86d9b9ede289f
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-20 04:09:02 +0800

    Initial Commit

---
+++ RestaurantPin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantPin
+//
+//  Created by yucian huang on 2024/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
